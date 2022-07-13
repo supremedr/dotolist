25621c7723c6f4845b2fb2686c6c996a701dc280
Ahmed Zaki <supremedr22@yahoo.com>
2022-07-13 20:19:23 +0200

    to heroku 2

--- dotolist/settings.py
+++ dotolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&xkz%@a-+)7k5p4bpc%_m!pz4p=o1-wjgd%kx+(erk2-0vlb5q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
