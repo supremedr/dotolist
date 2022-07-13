ee673565ac619393bfc25f60e4d6b334b69e4ce4
Ahmed Zaki <supremedr22@yahoo.com>
2022-07-13 20:49:30 +0200

    to heroku

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import User_task_rel, User_list_rel
+#from .models import User_task_rel, User_list_rel
 # Register your models here.
-admin.site.register([User_task_rel, User_list_rel])
+#admin.site.register([User_task_rel, User_list_rel])
--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,17 @@
-from django.db import models
-from django.contrib.auth.models import User
-from lists.models import Task, Lista
+#from django.db import models
+#from django.contrib.auth.models import User
+#from lists.models import Task, Lista
 
 
-class User_task_rel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    read = models.BooleanField(default=False)
-    update = models.BooleanField(default=False)
-    delete = models.BooleanField(default=False)
+#class User_task_rel(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    #read = models.BooleanField(default=False)
+    #update = models.BooleanField(default=False)
+    #delete = models.BooleanField(default=False)
 
 
-class User_list_rel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    lista = models.ForeignKey(Lista, on_delete=models.CASCADE)
-    create = models.BooleanField(default=False)
+#class User_list_rel(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #lista = models.ForeignKey(Lista, on_delete=models.CASCADE)
+    #create = models.BooleanField(default=False)
